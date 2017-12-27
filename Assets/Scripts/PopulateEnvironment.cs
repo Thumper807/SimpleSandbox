29d998214429994cb603aea6b726a97e56f5560f
Thumper807 <kevinbwidner@gmail.com>
2017-12-26 19:33:19 -0500

    WIP - Vegetation

--- Assets/Scripts/PopulateEnvironment.cs
+++ Assets/Scripts/PopulateEnvironment.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PopulateEnvironment : MonoBehaviour
 {
-    public GameObject[] Plants;
-    public int VegetationDensitiy = 10;
+    public GameObject[] PlantsToClone;
+    public int VegetationDensitiy = 100;
 
 	// Use this for initialization
 	void Start ()
@@ -15,23 +15,43 @@ public class PopulateEnvironment : MonoBehaviour
 	
 	private void Vegetate()
     {
+        Debug.Log("Beginning Vegetation Phase.");
         Vector3 groundSize = Vector3.Scale(transform.localScale, transform.GetComponent<MeshFilter>().mesh.bounds.size);
 
         for (int i = 0; i < VegetationDensitiy; i++)
         {
-            float randomX = Random.Range(-(groundSize.x / 2), groundSize.x / 2);
-            float randomZ = Random.Range(-(groundSize.z / 2), groundSize.z / 2);
-
-            Vector3 randomSpot = new Vector3(randomX, 0, randomZ);
-            Quaternion randomRotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
-
-            GameObject plant = Instantiate(getPlant(), randomSpot, randomRotation);
-            plant.transform.SetParent(transform, true);
+            SpawnPlant(groundSize);
         }
+        Debug.Log("Finished Vegetation Phase.");
     }
 
-    private GameObject getPlant()
+    private void SpawnPlant(Vector3 groundSize)
     {
-        return Plants[Random.Range(0, Plants.Length)];
+        Random.InitState(System.DateTime.Now.Millisecond);
+        float randomX = Random.Range(-(groundSize.x / 2), groundSize.x / 2);
+        float randomZ = Random.Range(-(groundSize.z / 2), groundSize.z / 2);
+        Vector3 randomSpot = new Vector3(randomX, 0, randomZ);
+
+        int failedAttempt = 0;
+        while (failedAttempt < 100 && Physics.CheckSphere(randomSpot, 5.0f, 9))
+        {
+            Random.InitState(System.DateTime.Now.Millisecond);
+            randomSpot.x = Random.Range(-(groundSize.x / 2), groundSize.x / 2);
+            randomSpot.z = Random.Range(-(groundSize.z / 2), groundSize.z / 2);
+
+            failedAttempt++;
+        }
+
+        if (failedAttempt == 100)
+            Debug.Log("Gave up finding a random spot.");
+
+        Quaternion randomRotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+        GameObject plant = Instantiate(GetPlant(), randomSpot, randomRotation);
+        plant.transform.SetParent(transform, true);
+    }
+
+    private GameObject GetPlant()
+    {
+        return PlantsToClone[Random.Range(0, PlantsToClone.Length)];
     }
 }
---
+++ Assets/Vegetation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vegetation : MonoBehaviour {
+
+    public float MinSize = 0.5f;
+    public float MaxSize = 2.0f;
+    public float MinGrowthRate = 0.5f;
+    public float MaxGrowthRate = 2.0f;
+    public bool Dead;
+    private float m_initialSize;
+    private float m_matureSize;
+    private float m_growthRate;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Random.InitState(System.DateTime.Now.Millisecond);
+        m_initialSize = m_matureSize * .25f;
+        m_matureSize = Random.Range(MinSize, MaxSize);
+        m_growthRate = Random.Range(MinGrowthRate, MaxGrowthRate);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
