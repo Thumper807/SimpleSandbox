9c3107598ab564848079acc848e55f238b684fcd
Thumper807 <kevinbwidner@gmail.com>
2018-01-01 11:23:08 -0500

    Fixed motion animation on inactive characters and added jumping animation

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,41 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class CharacterMovement : MonoBehaviour {
 
-    private Animator anim;
+    private Animator m_animator;
     public float WalkingSpeed = 1.75f;
-    public float fwdRunningSpeed = 5.0f;
-    public float MaxFwdSpeed = 5.0f;
-    public float RunningSpeedMultipler = 1.0f;
-    public float WalkingSpeedMultipler = 0.5f;
-    public float bwdSpeed = 5.0f;
-    public float rotationSpeed = 500.0f;
+    public float RotationSpeed = 500.0f;
     public bool Active;
 
     // Use this for initialization
     void Start () 
     {
-        anim = GetComponent<Animator>();
+        m_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update () 
     {
-        float characterMovement = 0.0f;
+        float xInput = 0.0f;
+        float yInput = 0.0f;
 
         if (Active)
-            characterMovement = Move();
-
-
-        // ANIMATE
-        if (Input.GetButtonDown("Jump"))
         {
-            anim.SetTrigger("isJumping");
+            Move(out xInput, out yInput);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                m_animator.SetTrigger("isJumping");
+            }
         }
+
+        m_animator.SetFloat("xMotion", xInput);
+        m_animator.SetFloat("yMotion", yInput);
     }
 
-    private float Move()
+    private void Move(out float xInput, out float yInput)
     {
         float yTranslation;
         float xTranslation;
@@ -45,17 +45,17 @@ public class CharacterMovement : MonoBehaviour {
         float rotation = 0.0f;
         if (Input.GetMouseButton(1))
         {
-            rotation = rotationSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
+            rotation = RotationSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
         }
-
+        
+        // 'left', 'right', 'a', and 'd' keys will rotate character (overrides the mouse)
         if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.0f)
         {
-            rotation = rotationSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+            rotation = RotationSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
         }
 
-        // 'fwd key' or 'Left & Right' mousebutton together will move character forward
-        // 'backward key' will move character back slowly
-        float yInput = Input.GetAxis("Vertical");
+        // 'fwd key' or 'backward key' will move character forward or backwards
+        yInput = Input.GetAxis("Vertical");
         float runInput = Input.GetAxis("RunModifier");
 
         yInput = yInput * (runInput + 1);
@@ -63,27 +63,8 @@ public class CharacterMovement : MonoBehaviour {
         yInput = Mathf.Clamp(yInput, -1.0f, 2.0f);
         yTranslation = yInput * WalkingSpeed * Time.deltaTime;
 
-        //if (Input.GetKey(KeyCode.W) || (Input.GetMouseButton(0) && Input.GetMouseButton(1)))
-        //{
-        //    float fwdSpeed = WalkingSpeed;
-        //    if (Input.GetKey(KeyCode.LeftShift))
-        //    {
-        //        fwdSpeed = fwdRunningSpeed;
-        //    }
-
-        //    translation = yInput * MaxFwdSpeed * Time.deltaTime;
-        //}
-        //else if (Input.GetKey(KeyCode.S))
-        //{
-        //    translation = -bwdSpeed * Time.deltaTime;
-        //}
-        //else
-        //{
-        //    translation = 0.0f;
-        //}
-
         // 'side keys' will strafe character left and right
-        float xInput = Input.GetAxis("Strafe");
+        xInput = Input.GetAxis("Strafe");
         xTranslation = xInput * WalkingSpeed * Time.deltaTime;
 
         // 'jump key' will cause character to jump
@@ -91,9 +72,5 @@ public class CharacterMovement : MonoBehaviour {
         // TRANSFORM
         transform.Translate(xTranslation, 0, yTranslation);
         transform.Rotate(Vector3.up, rotation);
-
-        anim.SetFloat("xMotion", xInput);
-        anim.SetFloat("yMotion", yInput);
-        return yTranslation;
     }
 }
--- Assets/Scripts/PopulateEnvironment.cs
+++ Assets/Scripts/PopulateEnvironment.cs
@@ -5,37 +5,54 @@ using UnityEngine;
 public class PopulateEnvironment : MonoBehaviour
 {
     public GameObject[] PlantsToClone;
+    public List<GameObject> Vegetation = new List<GameObject>();
     public int VegetationDensitiy = 100;
+    private Vector3 m_groundSize;
+    private int m_vegetationMask;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
+        Random.InitState(System.DateTime.Now.Millisecond);
+        m_groundSize = Vector3.Scale(transform.localScale, transform.GetComponent<MeshFilter>().mesh.bounds.size);
+        m_vegetationMask = 1 << 9;
+
         Vegetate();
 	}
 	
 	private void Vegetate()
     {
         Debug.Log("Beginning Vegetation Phase.");
-        Vector3 groundSize = Vector3.Scale(transform.localScale, transform.GetComponent<MeshFilter>().mesh.bounds.size);
 
         for (int i = 0; i < VegetationDensitiy; i++)
         {
-            SpawnPlant(groundSize);
+            Vegetation.Add(SpawnPlant(m_groundSize));
         }
+
         Debug.Log("Finished Vegetation Phase.");
     }
 
-    private void SpawnPlant(Vector3 groundSize)
+    private void Update()
     {
-        Random.InitState(System.DateTime.Now.Millisecond);
+        if (Vegetation.Count < VegetationDensitiy)
+        {
+            Vegetation.Add(SpawnPlant(m_groundSize));
+        }
+        else if (Vegetation.Count > VegetationDensitiy)
+        {
+            DeSpawnPlant();
+        }
+    }
+    private GameObject SpawnPlant(Vector3 groundSize)
+    {
         float randomX = Random.Range(-(groundSize.x / 2), groundSize.x / 2);
         float randomZ = Random.Range(-(groundSize.z / 2), groundSize.z / 2);
-        Vector3 randomSpot = new Vector3(randomX, 0, randomZ);
+        Vector3 randomSpot = new Vector3(randomX, 0.0f, randomZ);
 
         int failedAttempt = 0;
-        while (failedAttempt < 100 && Physics.CheckSphere(randomSpot, 5.0f, 9))
+
+        while (failedAttempt < 100 && Physics.CheckSphere(randomSpot, 0.5f, m_vegetationMask))
         {
-            Random.InitState(System.DateTime.Now.Millisecond);
             randomSpot.x = Random.Range(-(groundSize.x / 2), groundSize.x / 2);
             randomSpot.z = Random.Range(-(groundSize.z / 2), groundSize.z / 2);
 
@@ -48,10 +65,20 @@ public class PopulateEnvironment : MonoBehaviour
         Quaternion randomRotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
         GameObject plant = Instantiate(GetPlant(), randomSpot, randomRotation);
         plant.transform.SetParent(transform, true);
+
+        return plant;
     }
 
     private GameObject GetPlant()
     {
         return PlantsToClone[Random.Range(0, PlantsToClone.Length)];
+    }
+
+    private void DeSpawnPlant()
+    {
+        GameObject plant = Vegetation[Random.Range(0, Vegetation.Count)];
+        Vegetation.Remove(plant);
+
+        GameObject.Destroy(plant);
     }
 }
